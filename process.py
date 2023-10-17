adc8bdd02f8cb96abc54e65989ce162acf3f6c88
Jiri Vlasak <jirivlasak@gmail.com>
2023-10-17 14:23:35 +0200

    Refactoring

--- process.py
+++ process.py
@@ -4,13 +4,14 @@ import json
 import numpy as np
 import cv2
 import pprint
+import glob
 
 scalingFactor = 1.6
-landmarks_file = 'state.json'
-results_file = 'rtm-m-256x192/results_kolo_cerne.json'
+landmarks_file = 'gt/kolo_cerne.json'
+results_files = glob.glob("predictions/*/results_kolo_cerne.json")
 landmark_names = ["foot", "heel", "ankle",
                   "knee", "hip", "shoulder", "elbow", "wrist"]
-show = True
+show = False
 video_path = 'kolo_cerne.mp4'
 
 keypoint_names = ["left_small_toe", "left_heel","left_ankle", "left_knee", "left_hip", "left_shoulder", "left_elbow", "left_wrist"]
@@ -94,31 +95,33 @@ def visualizeLandmarks(landmarks, predictions, video_path):
 
 
 gt = loadJSON(landmarks_file)
+landmarks = getLandmarks(gt['$landmarksStore'])
 
-results = loadJSON(results_file)
+for results_file in results_files:
+    model_name = results_file.split('\\')[1]
+    print("Model: " + model_name)
 
-
-landmarks = getLandmarks(gt['landmarksStore'])
-print(len(landmarks[0]))
-
-predictions = results['instance_info']
-assert len(predictions) == len(landmarks)
-
-keypoint_mapping = getKeypointMapping(keypoint_names, results)
-print(keypoint_mapping)
-
-facing_left = isFacingLeft(landmarks)
-
-if not facing_left:
-    keypoint_mapping = flipKeypoints(keypoint_mapping, results)
+    results = loadJSON(results_file)
 
 
-distances = np.array(calcDistances(landmarks, predictions))
-landmarks_distances = np.mean(distances, axis=0)
 
-print("Landmarks distances: " + pprint.pformat(dict(zip(landmark_names, landmarks_distances))))
-print("Total avg distance: " + str(np.mean(landmarks_distances, where=landmarks_distances != -1)))
+    predictions = results['instance_info']
+    assert len(predictions) == len(landmarks)
+
+    keypoint_mapping = getKeypointMapping(keypoint_names, results)
+
+    facing_left = isFacingLeft(landmarks)
+
+    if not facing_left:
+        keypoint_mapping = flipKeypoints(keypoint_mapping, results)
 
 
-if show:
-    visualizeLandmarks(landmarks, predictions, video_path)
+    distances = np.array(calcDistances(landmarks, predictions))
+    landmarks_distances = np.mean(distances, axis=0)
+
+    print("Landmarks distances: \n" + pprint.pformat(dict(zip(landmark_names, landmarks_distances))))
+    print("Total avg distance: " + str(np.mean(landmarks_distances, where=landmarks_distances != -1)))
+
+    print()
+    if show:
+        visualizeLandmarks(landmarks, predictions, video_path)
