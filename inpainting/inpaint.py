c7db36ec22174bd40ed145bf94f5b1a79b138f68
Jiří Vlasák <jirivlasak9@gmail.com>
2024-02-11 23:52:51 +0100

    Add progress bar and save inpainted frames as jpg

--- inpainting/inpaint.py
+++ inpainting/inpaint.py
@@ -1,11 +1,20 @@
 import numpy as np
 import cv2 as cv
+import os
+from tqdm import tqdm
 
+filename = 'kolo_cerne.mp4'
+out_folder = "frames"
 # read video file
-cap = cv.VideoCapture('kolo_cerne.mp4')
+cap = cv.VideoCapture(filename)
 
+frame_idx = 0
 ret, frame = cap.read()
-while ret:
+# get number of frames
+num_frames = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
+
+# print progress bar
+for _ in tqdm(range(num_frames)):
     # convert to CIELAB color space
     lab = cv.cvtColor(frame, cv.COLOR_RGB2LAB)
     mask = np.zeros(frame.shape[:2], dtype=np.uint8)
@@ -20,8 +29,12 @@ while ret:
 
     dst = cv.inpaint(frame, mask, 15, cv.INPAINT_NS)
 
+    # save inpainted frame set jpg quality to 80
+    cv.imwrite(os.path.join(out_folder, f' {filename}_{frame_idx}.jpg'), dst, [
+               int(cv.IMWRITE_JPEG_QUALITY), 80])
     # cv.imshow("mask", mask)
-    cv.imshow('dst', dst)
-    if cv.waitKey(10) & 0xFF == ord('q'):
-        break
+    # cv.imshow('dst', dst)
+    # if cv.waitKey(10) & 0xFF == ord('q'):
+    #     break
     ret, frame = cap.read()
+    frame_idx += 1
