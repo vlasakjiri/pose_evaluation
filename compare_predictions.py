135971709b81d85b0fbdbda769286b81e8271cea
Jiri Vlasak <jirivlasak@gmail.com>
2024-03-28 10:37:46 +0100

    Added new data

--- compare_predictions.py
+++ compare_predictions.py
@@ -7,14 +7,14 @@ import pprint
 
 import numpy as np
 import pandas as pd
-
+from tqdm import tqdm
 from utils import *
 
 scalingFactor = 1.6
-landmarks_files = glob.glob("gt/10.json")
+landmarks_files = glob.glob("gt/*")
 landmark_names = ["foot", "heel", "ankle",
                   "knee", "hip", "shoulder", "elbow", "wrist"]
-show = True
+show = False
 video_path = "C:\\Users\\jiriv\\OneDrive\\bikefit videa\\val\\10.mp4"
 
 keypoint_names = ["left_small_toe", "left_heel", "left_ankle",
@@ -23,80 +23,74 @@ keypoint_names = ["left_small_toe", "left_heel", "left_ankle",
 
 def main():
     data = []
-    for landmarks_file in landmarks_files:
-        print("Landmarks file: " + landmarks_file)
+    for model in tqdm(glob.glob("predictions/*")):
+        print("Model: " + model)
+        model_distances = []
+        for image_prediction in tqdm(glob.glob(f"{model}/predictions/*")):
+            # print("Image prediction: " + image_prediction)
 
-        gt = loadJSON(landmarks_file)
-        landmarks = getLandmarks(gt['$landmarksStore'], scalingFactor)
-        facing_left = isFacingLeft(landmarks)
-        print("Facing left: " + str(facing_left))
+            # print(predictions)
+            # print()
+            basename= os.path.basename(image_prediction)
+            video_name = basename.split("_")[0]
+            frame_number = int(basename.split("_")[-1].split(".")[0])
 
-        landmarks = landmarksToArr(landmarks)
+            landmarks_file = f"gt\\{video_name}.json"
+            gt = loadJSON(landmarks_file)
+            landmarks = getLandmarks(gt['$landmarksStore'], scalingFactor)
+            facing_left = isFacingLeft(landmarks)
+            landmarks = landmarksToArr(landmarks)
 
-        base_filename = os.path.basename(landmarks_file).split(".")[0]
-
-        results_files = glob.glob(
-            f"results/*/predictions/{base_filename}.json")
-
-        for results_file in results_files:
-            model_name = results_file.split('\\')[1]
-            # print("Model: " + model_name)
-            # print("Results file: " + results_file)
-
-            results = loadJSON(results_file)
+            results = loadJSON(image_prediction)
             keypoint_mapping = getKeypointMapping(keypoint_names, results)
-
-            print(keypoint_mapping)
+            # print(keypoint_mapping)
             predictions = results['instance_info']
             # assert len(predictions) == len(landmarks)
             if not facing_left:
                 keypoint_mapping = flipKeypoints(keypoint_mapping, results)
-                print("Flipped keypoints", keypoint_mapping)
+                # print("Flipped keypoints", keypoint_mapping)
 
             predictions = predictionsToArr(predictions, keypoint_mapping)
 
-            # distances = calcDistancesArr(landmarks, predictions)
+            # get only the frame we are interested in
+            frame_landmarks = landmarks[frame_number]
+            frame_landmarks = frame_landmarks[np.newaxis, :]
 
-            # landmarks_distances = np.mean(distances, axis=0)
+            distances = calcDistancesArr(frame_landmarks, predictions).squeeze(0)
+            model_distances.append(distances)
+        
+        model_distances = np.mean(model_distances, axis=0)
+        distances_dict = dict(zip(landmark_names, model_distances))
+        print(distances_dict)
+        print()
+        data.append({
+            "model": model,
+            **distances_dict
+        })
+    return data
 
-            # distances_dict = dict(zip(landmark_names, landmarks_distances))
-
-            # data.append({
-            #     "model": model_name,
-            #     "video": video_path,
-            #     **distances_dict
-            # })
-
-            # print("Landmarks distances: \n" +
-            #       pprint.pformat(distances_dict, indent=4))
-            # print("Total avg distance: " +
-            #       str(np.mean(landmarks_distances, where=landmarks_distances != -1)))
-
-            # print()
-            if show:
-                visualizeLandmarks(landmarks, predictions, video_path)
-
-    df = pd.DataFrame(data)
-    return df
 
 
 if __name__ == "__main__":
-    df = main()
-    df = df.drop(columns="video").groupby("model").mean()
+    data = main()
+    df = pd.DataFrame(data)
+    df.to_csv("results.csv", index=False)
+    print(df)
+    # df = df.drop(columns="video").groupby("model").mean()
     # %%
 
-    all = df.drop(columns=["foot", "heel"])
-    all["mean"] = all.mean(axis=1)
-    all = all.sort_values(by="mean")
-    all.index = all.index.str.replace("_", "\_")
-    print(all)
-    # %%
-    wholebody = df.copy()
-    wholebody = wholebody[wholebody["foot"] != -1]
-    wholebody["mean"] = wholebody.mean(axis=1)
-    wholebody = wholebody.sort_values(by="mean")
-    wholebody.index = wholebody.index.str.replace("_", "\_")
-    print(wholebody.to_latex(float_format="%.2f"))
+    # all = df.drop(columns=["foot", "heel"])
+    # all["mean"] = all.mean(axis=1)
+    # all = all.sort_values(by="mean")
+    # all.index = all.index.str.replace("_", "\_")
+    # print(all)
+    # # %%
+    # wholebody = df.copy()
+    # wholebody = wholebody[wholebody["foot"] != -1]
+    # wholebody["mean"] = wholebody.mean(axis=1)
+    # wholebody = wholebody.sort_values(by="mean")
+    # wholebody.index = wholebody.index.str.replace("_", "\_")
+    # print(wholebody.to_latex(float_format="%.2f"))
 
 
 # %%
